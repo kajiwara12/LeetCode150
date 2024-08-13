6008590ebe2bfac4516bdf6122859a5c9287da6f
Quinn K <38480462+kajiwara12@users.noreply.github.com>
2024-08-13 06:43:45 -1000

    created file structure for LeetCode Questions

---
+++ Solutions/ArrayString/MergeSortedArray.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace LeetCode150
+{
+    public class Solution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            //Pointers
+            int i = m - 1;  
+            int j = n - 1;  
+            int k = m + n - 1;  
+
+           
+            for (int p = k; p >= 0; p--)
+            {
+                if (j < 0) // No more elements in nums2 to merge
+                {
+                    break;
+                }
+
+                if (i >= 0 && nums1[i] > nums2[j])
+                {
+                    nums1[p] = nums1[i];
+                    i--;
+                }
+                else
+                {
+                    nums1[p] = nums2[j];
+                    j--;
+                }
+            }
+        }
+    }
+}
---
+++ Tests/ArrayString/MergeSortedArrayTests/SolutionTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode150.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void MergeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
