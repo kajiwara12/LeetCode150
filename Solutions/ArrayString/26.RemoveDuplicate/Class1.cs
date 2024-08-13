3015c25f202af2111fb52370bea0e69bd2ed2fa3
Quinn K <38480462+kajiwara12@users.noreply.github.com>
2024-08-13 11:45:00 -1000

    added the problem# and added next leetcode problem

---
+++ Solutions/ArrayString/26.RemoveDuplicate/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace RemoveDuplicate
+{
+    public class Class1
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            // List to store unique elements
+            List<int> uniqueList = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                // Add the first element and all elements that are different from the previous one
+                if (i == 0 || nums[i] != nums[i - 1])
+                {
+                    uniqueList.Add(nums[i]);
+                }
+            }
+
+            // Copy the unique elements back to the original array
+            for (int i = 0; i < uniqueList.Count; i++)
+            {
+                nums[i] = uniqueList[i];
+            }
+            // Return the number of unique elements
+            return uniqueList.Count;
+        }
+    }
+}
