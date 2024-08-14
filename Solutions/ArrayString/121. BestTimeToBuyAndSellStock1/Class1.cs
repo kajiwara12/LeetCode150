45e569fa599bdafd7bfea191f36700e8ce80d97d
Quinn K <38480462+kajiwara12@users.noreply.github.com>
2024-08-14 06:19:29 -1000

    Added the next problem and test for it.

---
+++ Solutions/ArrayString/121. BestTimeToBuyAndSellStock1/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace _121._BestTimeToBuyAndSellStock1
+{
+    public class Class1
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int lowDay = 2147483647;
+            int difference = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < lowDay)
+                {
+                    lowDay = prices[i];
+                }
+                else
+                {
+                    difference = Math.Max(difference, prices[i] - lowDay);
+                }
+            }
+            return difference;
+        }
+    }
+}
---
+++ Tests/ArrayString/121. BestTimeToBuyAndSellStock1Tests/Class1Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _121._BestTimeToBuyAndSellStock1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _121._BestTimeToBuyAndSellStock1.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void MaxProfitTest()
+        {
+            int[] tester = { 7, 1, 5, 3, 6, 4 };
+            Class1 test = new();
+            int k = test.MaxProfit(tester);
+            Assert.AreEqual(5, k);
+        }
+    }
+}
