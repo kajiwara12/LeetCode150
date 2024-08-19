61cbbc15fc1e06737eb01f9e77eee8e041c9d9a7
Quinn K <38480462+kajiwara12@users.noreply.github.com>
2024-08-19 10:48:39 -1000

    added next set of problem and tests: ContainsDuplicate2

---
+++ Solutions/Hashmap/ContainsDuplicate2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashmap
+{
+    public class ContainsDuplicate2
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            Dictionary<int, int> map = new();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(nums[i]))
+                {
+                    int check = i - map[nums[i]];
+                    if (check <= k)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        map[nums[i]] = i;
+                    }
+                }
+                else
+                {
+                    map.Add(nums[i], i);
+                }
+            }
+            return false;
+        }
+        public bool ContainsNearbyDuplicate2(int[] nums, int k)
+        {
+            HashSet<int> map = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.TryGetValue(nums[i], out _))
+                {
+                    return true;
+                }
+                if (i >= k) 
+                { 
+                    map.Remove(nums[i-k]);
+                }
+                map.Add(nums[i]);
+            }
+            return false;
+
+        }
+    }
+
+}
---
+++ Tests/HashmapTests/ContainsDuplicate2Tests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hashmap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashmap.Tests
+{
+    [TestClass()]
+    public class ContainsDuplicate2Tests
+    {
+        [TestMethod()]
+        public void ContainsNearbyDuplicateTest()
+        {
+            ContainsDuplicate2 test = new();
+            int[] nums = new int[] { 1, 2, 3, 1, 2, 3 };
+            int k = 2;
+            bool result = test.ContainsNearbyDuplicate(nums, k);
+            Assert.IsFalse(result);
+        }
+        [TestMethod()]
+        public void ContainsNearbyDuplicateTest2()
+        {
+            ContainsDuplicate2 test = new();
+            int[] nums = new int[] { 1, 2, 3, 1 };
+            int k = 3;
+            bool result = test.ContainsNearbyDuplicate(nums, k);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void ContainsNearbyDuplicateTest3()
+        {
+            ContainsDuplicate2 test = new();
+            int[] nums = new int[] { 1, 0, 1, 1 };
+            int k = 1;
+            bool result = test.ContainsNearbyDuplicate(nums, k);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void ContainsNearbyDuplicate2Test()
+        {
+            ContainsDuplicate2 test = new();
+            int[] nums = new int[] { 1, 2, 3, 1, 2, 3 };
+            int k = 2;
+            bool result = test.ContainsNearbyDuplicate2(nums, k);
+            Assert.IsFalse(result);
+        }
+    }
+}
