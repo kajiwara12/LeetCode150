45c361a90ac43de8f08de69cb3b322c60fc335c3
Quinn K <38480462+kajiwara12@users.noreply.github.com>
2024-08-19 08:46:41 -1000

    added HappyNumbers problem and tests

---
+++ Solutions/Hashmap/HappyNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashmap
+{
+    public class HappyNumber
+    {
+        public bool IsHappy(int n)
+        {
+            HashSet<int> seen = new();
+            while (true)
+            {
+                int sum = 0;
+                while (n != 0)
+                {
+                    int last = n % 10;
+                    sum += last * last;
+                    n /= 10;
+                }
+                n = sum;
+                if (n == 1)
+                {
+                    return true;
+                }
+                if (seen.Contains(n))
+                {
+                    return false;
+                }
+                seen.Add(n);
+            }
+        
+        }
+    }
+}
+
---
+++ Tests/HashmapTests/HappyNumberTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hashmap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashmap.Tests
+{
+    [TestClass()]
+    public class HappyNumberTests
+    {
+        [TestMethod()]
+        public void IsHappyTest()
+        {
+            HappyNumber test = new();
+            int n = 19;
+            bool result =test.IsHappy(n);
+            Assert.IsTrue(result);
+        }
+        [TestMethod()]
+        public void IsHappyTest2()
+        {
+            HappyNumber test = new();
+            int n = 2;
+            bool result = test.IsHappy(n);
+            Assert.IsFalse(result);
+        }
+    }
+}
