53a2863421ff21cafbe16208977def635b7c10ee
Quinn K <38480462+kajiwara12@users.noreply.github.com>
2024-08-27 06:05:34 -1000

    added 2 more problems

---
+++ Solutions/ArrayString/HIndex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayString
+{
+    public class HIndexx
+    {
+        public int HIndex(int[] citations)
+        {
+            //int sum = 0;
+            //foreach (int i in citations) 
+            //{ 
+            //    sum += i;
+            //}
+
+            //return (int)Math.Floor(sum / (double)citations.Length);
+            Array.Sort(citations);
+            Array.Reverse(citations);
+            for (int i = 0; i < citations.Length; i++)
+            {
+                if (citations[i] <= i)
+                {
+                    return i;
+                }
+            }
+            return citations.Length;
+        }
+    }
+}
---
+++ Solutions/ArrayString/JumpGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayString
+{
+    public class JumpGame
+    {
+        public bool CanJump(int[] nums)
+        {
+            int currentPosition = 0;
+
+            while (nums[currentPosition] != 0) 
+            { 
+                int range = nums[currentPosition];
+                 var movementList =  nums[currentPosition..(currentPosition + range)];
+                currentPosition += movementList.Select((x, y) => x + y).Max();
+                if (currentPosition >= nums.Length) { return true; }
+            }
+            return false;
+        }
+    }
+}
